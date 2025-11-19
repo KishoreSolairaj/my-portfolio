f3ce922e0363b8411df26251c4f1d1ba4382f0b2
kishore.s <kishore.s@vinsinfo.com>
2025-11-19 11:47:21 +0530

    added pdf

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths - works everywhere
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
+  base: '/my-portfolio/', 
 })
